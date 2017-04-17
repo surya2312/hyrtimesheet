872f1ea1c911edd92f35fd59d1487fb82ec468c0
surya2312 <suryaprasad231@gmail.com>
2017-04-16 23:00:41 -0500

    cors changes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV !== 'production') {
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
